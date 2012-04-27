0435b5dda3e6d36ed7b78701ad538e6aec194ac9
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-04-28 02:30:22 +0300

    number comparing inside string is implemented

--- src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersComparator.java
+++ src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersComparator.java
@@ -1,10 +1,45 @@
 package com.gmail.eshustrov.comparator;
 
+import java.math.BigInteger;
 import java.util.Comparator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringWithNumbersComparator implements Comparator<String> {
+    private final Pattern PATTERN = Pattern.compile("(\\D*)(\\d*)");
+
     @Override
     public int compare(final String one, final String another) {
-        return one.compareTo(another);
+        final Matcher oneMatcher = PATTERN.matcher(one);
+        final Matcher anotherMatcher = PATTERN.matcher(another);
+        for (; ; ) {
+            oneMatcher.find();
+            anotherMatcher.find();
+
+            final String oneGroup = oneMatcher.group();
+            final String anotherGroup = anotherMatcher.group();
+            if (oneGroup.isEmpty() || anotherGroup.isEmpty()) {
+                return oneGroup.compareTo(anotherGroup);
+            }
+
+            final String oneTextGroup = oneMatcher.group(1);
+            final String anotherTextGroup = anotherMatcher.group(1);
+            if (!oneTextGroup.equals(anotherTextGroup)) {
+                return oneGroup.compareTo(anotherGroup);
+            }
+
+            final String oneNumberGroup = oneMatcher.group(2);
+            final String anotherNumberGroup = anotherMatcher.group(2);
+            if (oneNumberGroup.isEmpty() || anotherNumberGroup.isEmpty()) {
+                return oneNumberGroup.compareTo(anotherNumberGroup);
+            }
+
+            final BigInteger oneNumber = new BigInteger(oneNumberGroup);
+            final BigInteger anotherNumber = new BigInteger(anotherNumberGroup);
+            final int result = oneNumber.compareTo(anotherNumber);
+            if (result != 0) {
+                return result;
+            }
+        }
     }
 }
--- src/test/java/com/gmail/eshustrov/comparator/StringWithNumbersComparatorTest.java
+++ src/test/java/com/gmail/eshustrov/comparator/StringWithNumbersComparatorTest.java
@@ -9,6 +9,21 @@ public class StringWithNumbersComparatorTest {
     private final StringWithNumbersComparator comparator = new StringWithNumbersComparator();
 
     @Test
+    public void emptyStrings() {
+        assertThat(comparator.compare("", ""), is(0));
+    }
+
+    @Test
+    public void shorterStrings() {
+        assertThat(comparator.compare("", "a"), is(-1));
+    }
+
+    @Test
+    public void longerStrings() {
+        assertThat(comparator.compare("a", ""), is(1));
+    }
+
+    @Test
     public void equalStrings() {
         assertThat(comparator.compare("a", "a"), is(0));
     }
@@ -21,5 +36,35 @@ public class StringWithNumbersComparatorTest {
     @Test
     public void higherString() {
         assertThat(comparator.compare("b", "a"), is(1));
+    }
+
+    @Test
+    public void lowerStringByNumber() {
+        assertThat(comparator.compare("a2", "a10"), is(-1));
+    }
+
+    @Test
+    public void lowerStringNotByNumber() {
+        assertThat(comparator.compare("a10", "b2"), is(-1));
+    }
+
+    @Test
+    public void shorterStringAfterNumber() {
+        assertThat(comparator.compare("a0", "a0a"), is(-1));
+    }
+
+    @Test
+    public void lowerStringAfterNumber() {
+        assertThat(comparator.compare("a0a", "a0b"), is(-1));
+    }
+
+    @Test
+    public void lowerNumber() {
+        assertThat(comparator.compare("0", "1"), is(-1));
+    }
+
+    @Test
+    public void higherNumber() {
+        assertThat(comparator.compare("10", "2"), is(1));
     }
 }
