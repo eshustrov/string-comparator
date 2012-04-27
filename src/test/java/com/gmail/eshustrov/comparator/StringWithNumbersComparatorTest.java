66407b1abd5063a8ff7af43206a33ac22f631165
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-04-28 01:20:01 +0300

    text strings are test covered

--- src/test/java/com/gmail/eshustrov/comparator/StringWithNumbersComparatorTest.java
+++ src/test/java/com/gmail/eshustrov/comparator/StringWithNumbersComparatorTest.java
@@ -12,4 +12,14 @@ public class StringWithNumbersComparatorTest {
     public void equalStrings() {
         assertThat(comparator.compare("a", "a"), is(0));
     }
+
+    @Test
+    public void lowerString() {
+        assertThat(comparator.compare("a", "b"), is(-1));
+    }
+
+    @Test
+    public void higherString() {
+        assertThat(comparator.compare("b", "a"), is(1));
+    }
 }
