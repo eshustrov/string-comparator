4ff7f9e84e4b0b93655bfabe6ab19accd6848036
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-01 02:14:16 +0300

    Maxim's solution is added

---
+++ src/test/java/net/nowhere/maxim/comparator/NumericStringComparatorTest.java
@@ -0,0 +1,9 @@
+package net.nowhere.maxim.comparator;
+
+import common.CommonTest;
+
+public class NumericStringComparatorTest extends CommonTest {
+    public NumericStringComparatorTest() {
+        super(new NumericStringComparator());
+    }
+}
