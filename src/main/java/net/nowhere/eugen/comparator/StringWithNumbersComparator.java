46793c8a62d4f0d1b1dfbfa16342c06cba62474a
Jevgeni Holodkov <jevgeni.holodkov@gmail.com>
2012-05-01 22:25:25 +0300

    use charAt instead of explicit char[]

--- src/main/java/net/nowhere/eugen/comparator/StringWithNumbersComparator.java
+++ src/main/java/net/nowhere/eugen/comparator/StringWithNumbersComparator.java
@@ -11,21 +11,19 @@ public class StringWithNumbersComparator implements Comparator<String> {
     private final StringBuilder number2 = new StringBuilder();
 
     @Override
-    public int compare(String o1, String o2) {
-        char[] str1 = o1.toCharArray();
-        char[] str2 = o2.toCharArray();
+    public int compare(String str1, String str2) {
         int i = 0;
         int j = 0;
 
-        while (i < str1.length && j < str2.length) {
+        while (i < str1.length() && j < str2.length()) {
             // locate number in first string
             for (number1.setLength(0); isDigitAtIndex(str1, i); i++) {
-                number1.append(str1[i]);
+                number1.append(str1.charAt(i));
             }
 
             // locate number in second string
             for (number2.setLength(0); isDigitAtIndex(str2, j); j++) {
-                number2.append(str2[j]);
+                number2.append(str2.charAt(j));
             }
 
             if (number1.length() > 0 || number2.length() > 0) {
@@ -33,17 +31,17 @@ public class StringWithNumbersComparator implements Comparator<String> {
                 BigInteger n2 = parseBigInteger(number2);
                 if (!n1.equals(n2)) return n1.compareTo(n2);
             } else {
-                if (str1[i] != str2[j]) return str1[i] - str2[j];
+                if (str1.charAt(i) != str2.charAt(j)) return str1.charAt(i) - str2.charAt(j);
                 i++;
                 j++;
             }
         }
 
-        return str1.length - str2.length;
+        return str1.length() - str2.length();
     }
 
-    private boolean isDigitAtIndex(char[] str2, int j) {
-        return j < str2.length && Character.isDigit(str2[j]);
+    private boolean isDigitAtIndex(String str2, int j) {
+        return j < str2.length() && Character.isDigit(str2.charAt(j));
     }
 
     private BigInteger parseBigInteger(StringBuilder numberString) {
