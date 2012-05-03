f077610ef348e84c31b784092f0844e324fe20a7
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-04 01:46:28 +0300

    null paramaters are handled

--- src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersFastComparator.java
+++ src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersFastComparator.java
@@ -5,6 +5,16 @@ import java.util.Comparator;
 public class StringWithNumbersFastComparator implements Comparator<String> {
     @Override
     public int compare(final String one, final String another) {
+        if (one == null && another == null) {
+            return 0;
+        }
+        if (one == null) {
+            return -1;
+        }
+        if (another == null) {
+            return 1;
+        }
+
         return 0;
     }
 }
