d133a6a9f2e550d062cea3f04376278bc1e734ec
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-04 01:36:42 +0300

    new comparator is created

---
+++ src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersFastComparator.java
@@ -0,0 +1,10 @@
+package com.gmail.eshustrov.comparator;
+
+import java.util.Comparator;
+
+public class StringWithNumbersFastComparator implements Comparator<String> {
+    @Override
+    public int compare(final String one, final String another) {
+        return 0;
+    }
+}
---
+++ src/test/java/com/gmail/eshustrov/comparator/StringWithNumbersFastComparatorTest.java
@@ -0,0 +1,9 @@
+package com.gmail.eshustrov.comparator;
+
+import common.CommonTest;
+
+public class StringWithNumbersFastComparatorTest extends CommonTest {
+    public StringWithNumbersFastComparatorTest() {
+        super(new StringWithNumbersFastComparator());
+    }
+}
