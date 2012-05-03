04c13221262b4e554ad7ffbb806abc6f0bbce1a9
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-04 01:23:44 +0300

    reformatting

--- src/main/java/net/nowhere/maxim/comparator/NumericStringComparator.java
+++ src/main/java/net/nowhere/maxim/comparator/NumericStringComparator.java
@@ -9,31 +9,25 @@ public class NumericStringComparator implements Comparator<String> {
         if (o1.equals(o2) || o1.contentEquals(o2)) {
             return 0;
         }
+
         int minStringLength;
-        if(o1.length() <= o2.length())
-        {
+        if (o1.length() <= o2.length()) {
             minStringLength = o1.length();
-        }
-        else {
+        } else {
             minStringLength = o2.length();
         }
 
         for (AtomicInteger index = new AtomicInteger(0); index.get() < minStringLength; index.incrementAndGet()) {
-
             if (Character.isDigit(o1.charAt(index.get())) && Character.isDigit(o2.charAt(index.get()))) {
                 int initIndex1 = index.get();
                 int numberWeight1 = getNumberFromString(o1, index);
                 int numberWeight2 = getNumberFromString(o2, new AtomicInteger(initIndex1));
-                if (numberWeight1 != numberWeight2)
-                {
+                if (numberWeight1 != numberWeight2) {
                     return numberWeight1 > numberWeight2 ? 1 : -1;
                 }
-            }
-            else
-            {
+            } else {
                 int result = Character.valueOf(o1.charAt(index.get())).compareTo(o2.charAt(index.get()));
-                if(result != 0)
-                {
+                if (result != 0) {
                     return result;
                 }
             }
@@ -41,15 +35,13 @@ public class NumericStringComparator implements Comparator<String> {
         return o1.compareTo(o2);
     }
 
-    private int getNumberFromString(String stringWithNumbers, java.util.concurrent.atomic.AtomicInteger index)
-    {
+    private int getNumberFromString(String stringWithNumbers, AtomicInteger index) {
         int begin = index.intValue();
         int stringLength = stringWithNumbers.length();
-        while(Character.isDigit(stringWithNumbers.charAt(index.get()))){
-             if( stringLength <= index.incrementAndGet()) break;
+        while (Character.isDigit(stringWithNumbers.charAt(index.get()))) {
+            if (stringLength <= index.incrementAndGet()) break;
         }
 
-        return Integer.parseInt(stringWithNumbers.substring(begin,index.intValue()));
+        return Integer.parseInt(stringWithNumbers.substring(begin, index.intValue()));
     }
-
 }
