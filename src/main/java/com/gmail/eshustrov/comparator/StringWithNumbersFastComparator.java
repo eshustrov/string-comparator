dde74ca1553ee7eb525aa6b57ba6ffcf4bcf86bf
Eduard Shustrov <eduard.shustrov@elisa.ee>
2012-05-04 20:13:38 +0300

    string comparison without taking numbers into account is implemented

--- src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersFastComparator.java
+++ src/main/java/com/gmail/eshustrov/comparator/StringWithNumbersFastComparator.java
@@ -15,6 +15,26 @@ public class StringWithNumbersFastComparator implements Comparator<String> {
             return 1;
         }
 
+        final int oneLength = one.length();
+        final int anotherLength = another.length();
+        final int minLength = oneLength < anotherLength ? oneLength : anotherLength;
+        for (int index = 0; index < minLength; index++) {
+            final char oneSymbol = one.charAt(index);
+            final char anotherSymbol = another.charAt(index);
+            if (oneSymbol < anotherSymbol) {
+                return -1;
+            }
+            if (oneSymbol > anotherSymbol) {
+                return 1;
+            }
+        }
+
+        if (oneLength < anotherLength) {
+            return -1;
+        }
+        if (oneLength > anotherLength) {
+            return 1;
+        }
         return 0;
     }
 }
